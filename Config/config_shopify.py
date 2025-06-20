373b5205cfa36a15c277e3ddf830ea189789f13e
dkfbayraktar <dkfbayraktar@gmail.com>
2025-06-20 22:29:39 +0300

    Update config_shopify.py

--- Config/config_shopify.py
+++ Config/config_shopify.py
@@ -3,12 +3,16 @@ import os
 # Shopify Mağaza Domain ve Token bilgileri
 SHOPIFY_STORES = [
     {
-        "domain": "www.thenichebox.co",
-        "access_token": os.getenv('SHOPIFY_API_ACCESS_TOKEN')
+        "SHOPIFY_DOMAIN": os.getenv("SHOPIFY_DOMAIN"),
+        "SHOPIFY_API_KEY": os.getenv("SHOPIFY_API_KEY"),
+        "SHOPIFY_API_SECRET_KEY": os.getenv("SHOPIFY_API_SECRET_KEY"),
+        "SHOPIFY_API_ACCESS_TOKEN": os.getenv("SHOPIFY_API_ACCESS_TOKEN"),
     },
     # Daha fazla mağaza eklemek için aşağıdaki örneği kullanın:
     # {
-    #     "domain": "your-second-store.myshopify.com",
-    #     "access_token": os.getenv('SHOPIFY_API_ACCESS_TOKEN_2')
+    #     "SHOPIFY_DOMAIN_2": os.getenv("SHOPIFY_DOMAIN_2"),
+    #     "SHOPIFY_API_KEY_2": os.getenv("SHOPIFY_API_KEY_2"),
+    #     "SHOPIFY_API_SECRET_KEY_2": os.getenv("SHOPIFY_API_SECRET_KEY_2"),
+    #     "SHOPIFY_API_ACCESS_TOKEN_2": os.getenv("SHOPIFY_API_ACCESS_TOKEN_2")
     # },
 ] 
